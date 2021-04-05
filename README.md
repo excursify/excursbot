ef713a7760ff88432ab19549e776644495feb5d2
excursify <excursify08@gmail.com>
2021-04-05 16:49:11 +0800

    added ping command and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Excursbot
 
-Lole xd
+Personal bot just to test and learn stuff
 
-*It has simple commands like ping. Other commands are to follo :smile:*
+*It has simple commands like ping. Other commands are to follow :smile:.*
--- src/excursbot.js
+++ src/excursbot.js
@@ -1,6 +1,5 @@
 import {BOT_USERNAME, CHANNEL_NAME, O_AUTH} from './login'
-
-
+const tmi = require("tmi.js");
 const { ChatClient } = require("dank-twitch-irc");
 
 let client = new ChatClient(
@@ -10,6 +9,7 @@ let client = new ChatClient(
   }
 )
 
+client.on("connecting", () => client.say(CHANNEL_NAME,"Trying to join PauseChamp"))
 client.on("ready", () => joinedlole());
 client.on("close", (error) => {
   if (error != null) {
@@ -21,12 +21,46 @@ client.on("PRIVMSG", (msg) => {
   console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
 });
 
+// Joining Channel
 client.connect();
 client.join(CHANNEL_NAME);
 
-
+// Random Funcion
 function joinedlole(){
   console.log("Successfully connected to chat");
-  client.say(CHANNEL_NAME, "I joined PagMan");
+  client.say(CHANNEL_NAME, "Success PagMan 🤘 ");
 }
 
+
+// Colors
+client.setColor({r: 95, g: 158, b: 160});
+// Ping Command
+/*client.on("message", (msg) => {
+  if (msg.messageText === "^ping"){
+    client.say(CHANNEL_NAME, "Pong! 🏓" )
+  }
+}) */
+
+client.on("message", (msg) => {
+  if (msg.messageText === "^hello"){
+    client.say(CHANNEL_NAME," 👋 Okayge " + msg.displayName)
+  }
+})
+
+function timeformat(seconds){
+  function pad(s){
+      return (s < 10 ? '0' : '') + s;
+  };
+  var hours = Math.floor(seconds/ (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  
+  return pad(hours) + ':' + pad(minutes) + ":" + pad(seconds);
+}
+
+client.on("message", (msg) => {
+
+  if (msg.messageText === "^ping"){
+    client.say(CHANNEL_NAME,"Pong! FeelsDankMan 🏓 Uptime: " + timeformat(process.uptime()))
+  }
+})   
