ae9d8c4ed1e6f23450a30f43e58a4f3e2886d49f
excursify <excursify08@gmail.com>
2021-04-30 12:43:43 +0800

    Test lole

---
+++ src/commands/4head.js
@@ -0,0 +1,11 @@
+// random 4head jokes 
+/*
+bot.ebot.on("message", (msg) => {
+    if (!msg.messageText) return;
+    if (msg.messageText.startsWith("^4Head")){
+        // a database and pulls out a random string from that database
+
+    }
+})*/
+
+// This is a test lole xd 
